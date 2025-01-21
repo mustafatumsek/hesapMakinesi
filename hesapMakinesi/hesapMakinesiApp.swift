32d2d1929309ac411d9796700f6187a1b016fda8
Mustafa <mtumsek22@posta.pau.edu.tr>
2025-01-21 18:08:26 +0300

    Initial Commit

---
+++ hesapMakinesi/hesapMakinesiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hesapMakinesiApp.swift
+//  hesapMakinesi
+//
+//  Created by Mustafa Tümsek on 21.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct hesapMakinesiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hesapMakinesiTests/hesapMakinesiTests.swift
@@ -0,0 +1,17 @@
+//
+//  hesapMakinesiTests.swift
+//  hesapMakinesiTests
+//
+//  Created by Mustafa Tümsek on 21.01.2025.
+//
+
+import Testing
+@testable import hesapMakinesi
+
+struct hesapMakinesiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
